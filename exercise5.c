3a3f59d8470d5617000fc5d73a431775e2559cb7
Mr.Sharvin <93881528+SharvinKumarArumugam@users.noreply.github.com>
2022-04-02 21:39:58 +0800

    Update exercise5.c

--- exercise5.c
+++ exercise5.c
@@ -10,7 +10,19 @@
 //main function section 
 int main(){
     //write here your program
+    int n,i,avg[5];
 
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter the Value : ");
+        scanf("%d",&n);
+
+    }
+
+
+    avg[5]=n/5;
+
+        printf("\nAverag value is %d ",avg[5]);
 
     return 0;
 }
