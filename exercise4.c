0360d80a3acb704628d724a3484ad7e50ff11299
Mr.Sharvin <93881528+SharvinKumarArumugam@users.noreply.github.com>
2022-04-02 21:11:01 +0800

    Update exercise4.c

--- exercise4.c
+++ exercise4.c
@@ -11,7 +11,19 @@
 int main(){
     //write here your program
 
+    int theValue[3][3];
 
+    theValue[0][0]= 10;
+    theValue[0][1]= 100;
+    theValue[0][2]= 20;
+    theValue[1][0]= 50;
+    theValue[1][1]= 20;
+    theValue[1][2]= 40;
+    theValue[2][0]= 40;
+    theValue[2][1]= 40;
+    theValue[2][2]= 10;
+
+    printf("%d",theValue[1][2]);
     return 0;
 }
 
